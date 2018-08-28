8402c51bd2bbc2bc7bf9231fe64b7af2ab5e0953
Jacob Bleser <jacobbleser@aol.com>
2018-08-28 07:25:43 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hi there, you've reached the line of cloudless!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
